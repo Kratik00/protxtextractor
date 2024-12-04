bb5b97699ed30d8b4789a1176823eac867462d9f
Kratik00 <159747619+Kratik00@users.noreply.github.com>
2024-12-04 16:38:56 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ PASS_DB = int(os.environ.get("PASS_DB", "721"))
 
 OWNER = int(os.environ.get("OWNER", "7145429939"))
 
-LOG = ,
+LOG = int(os.environ.get("LOG", "0")),
 
 # UPDATE_GRP = , # bot sat group
 
