4af2e0bb3f58030e82e30d8696a5a108ff93cd7d
Kratik00 <159747619+Kratik00@users.noreply.github.com>
2024-12-04 22:29:34 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,8 +10,6 @@ PASS_DB = int(os.environ.get("PASS_DB", "721"))
 
 OWNER = int(os.environ.get("OWNER", ))
 
-LOG = int(os.environ.get("LOG", "0")),
-
 # UPDATE_GRP = , # bot sat group
 
 # auth_chats = []
